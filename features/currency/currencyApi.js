2256e93d7bde57bd7f9d3523287d5b619f8eddaf
Alamgirhossann <alamgirhossann@gmail.com>
2023-04-22 17:55:13 +0600

    api update

--- features/currency/currencyApi.js
+++ features/currency/currencyApi.js
@@ -1,7 +1,7 @@
 
 
 export const getCurrency = async () => {
- const res = await fetch('https://api.apilayer.com/fixer/latest?base=USD&apikey=ieXzxGLEepiF2xZgIgitT9gDbmQvocN4555')
+ const res = await fetch('https://api.apilayer.com/fixer/latest?base=USD&apikey=ZjqB0ixLE8luEblQFtLgMxT1CM7Y3pXh')
   const data = await res.json()
   return data;
 };
